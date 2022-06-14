7fd0242579abc0240fd08567706cb043f40d590a
YK Dong <ykdong1991@gmail.com>
2022-06-14 17:51:30 -0400

    add loader token

--- server/router.js
+++ server/router.js
@@ -4,5 +4,7 @@ const router = require('express').Router()
 router.get('/products/:product_id', controller.getProductInfo)
 router.get('/products/:product_id/styles', controller.getStyles)
 router.get('/products/:product_id/related', controller.getRelated)
+router.get('/loaderio-7190a7dbb03ad1b0b862f7fb22d1ca8d.txt', (req, res) => 
+  res.status(200).send('../loaderio-7190a7dbb03ad1b0b862f7fb22d1ca8d.txt'));
 
 module.exports = router
