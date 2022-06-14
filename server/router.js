0787602ab92da6e5c6298aa4c3da3ba2b1a8150f
YK Dong <ykdong1991@gmail.com>
2022-06-14 17:56:25 -0400

    bug fix for the loader.io(can not send back the token)

--- server/router.js
+++ server/router.js
@@ -5,6 +5,6 @@ router.get('/products/:product_id', controller.getProductInfo)
 router.get('/products/:product_id/styles', controller.getStyles)
 router.get('/products/:product_id/related', controller.getRelated)
 router.get('/loaderio-7190a7dbb03ad1b0b862f7fb22d1ca8d.txt', (req, res) => 
-  res.status(200).send('../loaderio-7190a7dbb03ad1b0b862f7fb22d1ca8d.txt'));
+  res.status(200).send('loaderio-7190a7dbb03ad1b0b862f7fb22d1ca8d'));
 
 module.exports = router
