f1dcc8dde092e43bb94f7fdaee631998c26185d2
YK Dong <ykdong1991@gmail.com>
2022-06-16 12:45:21 -0400

    update loader.io varify token

--- server/router.js
+++ server/router.js
@@ -4,7 +4,7 @@ const router = require('express').Router()
 router.get('/products/:product_id', controller.getProductInfo)
 router.get('/products/:product_id/styles', controller.getStyles)
 router.get('/products/:product_id/related', controller.getRelated)
-router.get('/loaderio-53717b1d5c16264945feeab87d0127b2.txt', (req, res) => 
-  res.status(200).send('loaderio-53717b1d5c16264945feeab87d0127b2'));
+router.get('/loaderio-46d014e68b41f604d0800fecf3f0902d.txt', (req, res) => 
+  res.status(200).send('loaderio-46d014e68b41f604d0800fecf3f0902d'));
 
 module.exports = router
